0521fbdaedd30162b35a70b0ba34a901e75e10b9
laynnebeatriz <beatriz132@gmail.com>
2025-05-27 11:13:48 -0300

    Aula 13 - Curso Java Basico Loiane - Exercicio2

---
+++ src/main/java/aula13/Labs2.java
@@ -0,0 +1,15 @@
+package aula13;
+
+import java.util.Scanner;
+
+public class Labs2 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com um numero inteiro");
+        int numero = scan.nextInt();
+        System.out.println("O numero informado foi: " + numero);
+
+    }
+}
