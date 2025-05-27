29c55696bbb4cce618d458f0f587fce0f367b863
laynnebeatriz <beatrizmorais132@gmail.com>
2025-05-27 17:05:21 -0300

    Aula 13 - Curso Java Basico Loiane - Labs5

---
+++ src/main/java/aula13/Labs5.java
@@ -0,0 +1,17 @@
+package aula13;
+
+import java.util.Scanner;
+
+public class Labs5 {
+    public static void main(String[] args) {
+
+        Scanner scan  = new Scanner(System.in);
+
+        System.out.println("Entre com a quantidade de metros");
+        double metros = scan.nextDouble();
+
+        double cm = metros * 100;
+
+        System.out.println(metros + " m é igual a" + cm + " cm");
+    }
+}
