94ade60c552c66f071a44503ca479125f10a3774
laynnebeatriz <beatrizmorais132@gmail.com>
2025-06-17 11:52:29 -0300

    Video Aula Loiane - Exercicios das aulas 16 e 17 : Exerc02

--- src/main/java/ExerAula16e17/Exerc02.java
+++ src/main/java/ExerAula16e17/Exerc02.java
@@ -9,16 +9,16 @@ public class Exerc02 {
 
         boolean infoValidas = false;
         String nomeUser;
-        String senha;
+        String senha1;
 
         do {
             System.out.println("Entre com o  nome do usuario:");
             nomeUser = scan.next();
 
             System.out.println("Entre com a senha:");
-            senha = scan.next();
+            senha1 = scan.next();
 
-            if (nomeUser.equalsIgnoreCase(senha)){
+            if (nomeUser.equalsIgnoreCase(senha1)){
                 //infoValidas = false;
                 System.out.println("Senha igual ao usuario, digite novamente:");
             } else {
