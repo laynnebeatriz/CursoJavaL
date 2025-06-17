6b85a2b9ae61ba9660bdfdba549d9f1630da8563
laynnebeatriz <beatrizmorais132@gmail.com>
2025-06-17 11:10:32 -0300

    Video Aula Loiane - Exercicios das aulas 16 e 17 : Exerc01

---
+++ src/main/java/ExerAula16e17/Exerc01.java
@@ -0,0 +1,26 @@
+package ExerAula16e17;
+
+import java.util.Scanner;
+
+public class Exerc01 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        boolean notaValida = false;
+
+
+        do {
+            System.out.println("Entre com uma nota");
+
+            double nota = scan.nextDouble();
+
+            if (nota >= 0 && nota <= 10) {
+                notaValida = true;
+                System.out.println("Você digitou:" + nota);
+            } else {
+                System.out.println(" Nota invalida, digite novamente.");
+            }
+        } while (!notaValida);
+    }
+}
