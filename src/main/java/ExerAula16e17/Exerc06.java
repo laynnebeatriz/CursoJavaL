1f98b6dc992fdb3da9ab4d4b2fa40976e739b0c5
laynnebeatriz <beatrizmorais132@gmail.com>
2025-06-18 15:26:25 -0300

    Video Aula Loiane - Exercicios das aulas 16 e 17 : Exerc06

---
+++ src/main/java/ExerAula16e17/Exerc06.java
@@ -0,0 +1,11 @@
+package ExerAula16e17;
+
+public class Exerc06 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++){
+            //System.out.println(i);
+            System.out.print(i + "");
+        }
+    }
+}
