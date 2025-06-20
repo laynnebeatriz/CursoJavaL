19753e964609091a63faf23917622c37de9cad03
laynnebeatriz <beatrizmorais132@gmail.com>
2025-06-20 15:38:11 -0300

    Video Aula Loiane - Exercicios das aulas 16 e 17 : Exerc24

---
+++ src/main/java/ExerAula16e17/Exerc24.java
@@ -0,0 +1,14 @@
+package ExerAula16e17;
+
+public class Exerc24 {
+    public static void main(String[] args) {
+
+        System.out.println("Preço do pão: R$ 0.18");
+        System.out.println("Preço do pão de Otem - Tabela de preços");
+
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i + " - R$ " + (0.18*i));
+
+        }
+    }
+}
