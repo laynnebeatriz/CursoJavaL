e598595d0b3dd463549d28239d62d67b51e30bce
laynnebeatriz <beatrizmorais132@gmail.com>
2025-06-20 15:37:59 -0300

    Video Aula Loiane - Exercicios das aulas 16 e 17 : Exerc23

---
+++ src/main/java/ExerAula16e17/Exerc23.java
@@ -0,0 +1,16 @@
+package ExerAula16e17;
+
+import java.util.Scanner;
+
+public class Exerc23 {
+    public static void main(String[] args) {
+
+        //Scanner scan = new Scanner(System.in);
+
+        System.out.println("Lojas Quase Dois - Tabelas de preços");
+
+        for (int i =1; i <= 50; i++){
+            System.out.println(i + " - R$ " + (1.99*i));
+        }
+    }
+}
