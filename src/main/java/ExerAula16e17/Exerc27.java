f4cdf502e18ee23acd6aec2881833080b4903f49
laynnebeatriz <beatrizmorais132@gmail.com>
2025-06-21 12:18:29 -0300

    Video Aula Loiane - Exercicios das aulas 16 e 17 : Exerc27

---
+++ src/main/java/ExerAula16e17/Exerc27.java
@@ -0,0 +1,35 @@
+package ExerAula16e17;
+
+import java.util.Scanner;
+
+public class Exerc27 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com a qtd de temperaturas: ");
+        int qtdTemperaturas = scan.nextInt();
+
+        double temp;
+        double soma = 0;
+        double menor = Double.MAX_VALUE;
+        double maior = Double.MIN_VALUE;
+
+        for (int i = 1; i <= qtdTemperaturas; i++) {
+            System.out.println("Entre com a temperatura " + i);
+            temp = scan.nextDouble();
+
+            soma += temp;
+
+            if (temp > maior) {
+                maior = temp;
+            }
+            if (temp < menor) {
+                menor = temp;
+            }
+        }
+        System.out.println("Média: " + (soma / qtdTemperaturas));
+        System.out.println("Menor Temperatura: " + menor);
+        System.out.println("Maior Temperatura: " + maior);
+    }
+}
