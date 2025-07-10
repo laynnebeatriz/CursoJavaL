4df0bb4e523a628615d9a2d05a9f6b910154f924
laynnebeatriz <beatrizmorais132@gmail.com>
2025-07-10 15:51:35 -0300

    Video Aula Loiane - Aula de Vetores (Arrays) - Aula19Exerc11

---
+++ src/main/java/Vetores/Aula19Exerc11.java
@@ -0,0 +1,29 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class Aula19Exerc11 {
+    public static void main(String[] args) {
+
+        Scanner scan  = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+
+        for (int i=0; i<vetorA.length; i++){
+            System.out.println("Entre com o valor da posição: " + i);
+            vetorA[i] = scan.nextInt();
+        }
+        int qtdPares = 0;
+        for (int i=0; i<vetorA.length; i++){
+            if (vetorA[i] % 2 == 0){
+                qtdPares++;
+            }
+        }
+        System.out.print("Vetor A = ");
+        for (int i=0; i<vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Quantidade de pares: " + qtdPares);
+    }
+}
