03c2ed029ba63cf485879da3b8d8c53676fd6272
laynnebeatriz <beatrizmorais132@gmail.com>
2025-07-10 12:19:25 -0300

    Video Aula Loiane - Aula de Vetores (Arrays) - Aula19Exerc03

---
+++ src/main/java/Vetores/Aula19Exerc3.java
@@ -0,0 +1,31 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class Aula19Exerc3 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] vetorA = new int[15];
+        int[] vetorB = new int[vetorA.length];
+
+        for (int i=0; i<vetorA.length; i++) {
+            System.out.println("Entre com o valor da posição: " + i);
+            vetorA[i] = scan.nextInt();
+
+            vetorB[i] = vetorA[i] * vetorA[i];
+        }
+        System.out.print("Vetor A = ");
+        for (int i=0; i<vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Vetor B = ");
+        for (int i=0; i<vetorB.length; i++) {
+            System.out.print(vetorB[i] + " ");
+        }
+        System.out.println();
+    }
+}
