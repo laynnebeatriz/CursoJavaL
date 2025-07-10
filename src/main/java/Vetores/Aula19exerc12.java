e6dc1a5184cfdaab9b0cd2e7641965f4c630ab8c
laynnebeatriz <beatrizmorais132@gmail.com>
2025-07-10 15:51:47 -0300

    Video Aula Loiane - Aula de Vetores (Arrays) - Aula19Exerc12

---
+++ src/main/java/Vetores/Aula19exerc12.java
@@ -0,0 +1,27 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class Aula19exerc12 {
+    public static void main(String[] args) {
+
+        Scanner scan  = new Scanner(System.in);
+
+        int[] vetorA = new int[10];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println("Entre com o valor da posição: " + i);
+            vetorA[i] = scan.nextInt();
+        }
+        int soma = 0;
+        for (int i = 0; i < vetorA.length; i++) {
+            soma += vetorA[i];
+        }
+        System.out.print("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.print(vetorA[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Soma: " + soma);
+    }
+}
