b4e05e498fcf99b940dc1f1f2b352a1b0254863f
laynnebeatriz <beatrizmorais132@gmail.com>
2025-07-10 15:52:48 -0300

    Video Aula Loiane - Aula de Vetores (Arrays) - Aula19Exerc17

---
+++ src/main/java/Vetores/Aula19Exerc17.java
@@ -0,0 +1,29 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class Aula19Exerc17 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] idades = new int[10];
+
+        for (int i = 0; i < idades.length; i++) {
+            System.out.println("Entre com a idade da pessoa: " + (i+1));
+            idades[i] = scan.nextInt();
+        }
+        int qtd = 0;
+        for (int i = 0; i < idades.length; i++){
+            if (idades[i] > 35){
+                qtd++;
+            }
+        }
+        System.out.print("Vetor de idades = ");
+        for (int i = 0; i < idades.length; i++) {
+            System.out.print(idades[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Qtd pessoas idade > 35: " + qtd);
+    }
+}
