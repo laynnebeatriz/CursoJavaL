faed8f6ca2e134d570c96f532f4cf065fbb86111
laynnebeatriz <beatrizmorais132@gmail.com>
2025-08-11 18:01:14 -0300

    Video Aula Loiane - Aula 24 - Orientação a Objetos - Exercicios 06

---
+++ src/main/java/Aula24Exercicio/ContaCorrente.java
@@ -0,0 +1,11 @@
+package Aula24Exercicio;
+
+public class ContaCorrente {
+
+    String numero;
+    String agencia;
+    boolean especial;
+    double limiteEspecial;
+    double saldo;
+
+}
---
+++ src/main/java/Aula24Exercicio/Exerc5ContaCorrente.java
@@ -0,0 +1,15 @@
+package Aula24Exercicio;
+
+public class Exerc5ContaCorrente {
+    public static void main(String[] args) {
+
+        ContaCorrente conta = new ContaCorrente();
+        conta.numero = "123456";
+        conta.agencia = "1234";
+        conta.especial = true;
+        conta.limiteEspecial = 500;
+        conta.saldo = -10;
+
+        System.out.println("Saldo da conta " + conta.numero + " = " + conta.saldo);
+    }
+}
