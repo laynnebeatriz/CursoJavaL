df938e3a1254287c2d40e54916f6850ba97bab79
laynnebeatriz <beatrizmorais132@gmail.com>
2025-08-11 17:59:32 -0300

    Video Aula Loiane - Aula 24 - Orientação a Objetos - Exercicios 01

---
+++ src/main/java/Aula24Exercicio/Lampada.java
@@ -0,0 +1,16 @@
+package Aula24Exercicio;
+
+public class Lampada {
+
+    String modelo;
+    String tensao;
+    String cor;
+    String tipoLuz;
+    int potencia;
+    int garantiaMeses;
+    String[] tipos;
+    boolean tipoAbajur;
+    String codigoDeBarra;
+    Boolean disponivel;
+    double preco;
+}
