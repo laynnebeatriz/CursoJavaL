05f0be1aa046dd4a48cf34fd89c9d85c21e9c916
laynnebeatriz <beatrizmorais132@gmail.com>
2025-08-11 17:59:55 -0300

    Video Aula Loiane - Aula 24 - Orientação a Objetos - Exercicios 02

---
+++ src/main/java/Aula24Exercicio/Livro.java
@@ -0,0 +1,10 @@
+package Aula24Exercicio;
+
+public class Livro {
+
+    String nome;
+    String autor;
+    int qtdPaginas;
+    int anoLancamento;
+    String isbn;
+}
---
+++ src/main/java/Aula24Exercicio/VendaLivro.java
@@ -0,0 +1,14 @@
+package Aula24Exercicio;
+
+public class VendaLivro {
+    public static void main(String[] args) {
+
+        Livro livro = new Livro();
+
+        livro.nome = "Mastering ExtJS";
+        livro.autor = "Loiane Groner";
+        livro.anoLancamento = 2015;
+
+        System.out.println("Nome do livro = " + livro.nome);
+    }
+}
