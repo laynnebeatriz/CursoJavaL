e6b924af6ad215ca3b7314dfa3d567f3b657957d
laynnebeatriz <beatrizmorais132@gmail.com>
2025-08-26 17:13:49 -0300

    alt Modificadores private public - Aula 31 (Orientação a Objetos)

---
+++ src/main/java/Aula31alt/TesteCarroAlt31.java
@@ -0,0 +1,12 @@
+package Aula31alt;
+
+import Aula31.Carro31;
+
+public class TesteCarroAlt31 {
+    public static void main(String[] args) {
+
+        Carro31 carro = new Carro31();
+        carro.marca = "Fiat";
+
+    }
+}
