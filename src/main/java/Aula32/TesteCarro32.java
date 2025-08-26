4bfa731525f98ffc0efdb87586a835db772f518f
laynnebeatriz <beatrizmorais132@gmail.com>
2025-08-26 17:17:24 -0300

    Encapsulamento: metodos Getters e setters - Aula 33 (Orientação a Objetos)

---
+++ src/main/java/Aula32/TesteCarro32.java
@@ -0,0 +1,12 @@
+package Aula32;
+
+public class TesteCarro32 {
+    public static void main(String[] args) {
+
+        Carro32 van = new Carro32();
+
+        van.setMarca("Fiat");
+
+        System.out.println(van.getMarca());
+    }
+}
