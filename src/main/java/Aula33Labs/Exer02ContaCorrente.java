15654b746469bc02a65d6c5997a548c7afb93802
laynnebeatriz <beatrizmorais132@gmail.com>
2025-08-28 19:38:59 -0300

    02 - Correção dos exercicios da aula 33 - OO Conta Corrente

---
+++ src/main/java/Aula33Labs/Exer02ContaCorrente.java
@@ -0,0 +1,49 @@
+package Aula33Labs;
+
+public class Exer02ContaCorrente {
+    public static void main(String[] args) {
+
+        ContaCorrente conta = new ContaCorrente();
+        conta.setNumero("123456");
+        conta.setAgencia("1234");
+        conta.setEspecial(true);
+        conta.setLimiteEspecial(500);
+        conta.setSaldo(-10);
+
+        System.out.println("Saldo da conta " + conta.getNumero() + " = " + conta.getSaldo());
+
+        boolean saqueEfetuado = conta.realizarSaque(10);
+
+        if (saqueEfetuado){
+            System.out.println("Saque efetuado com sucesso");
+            conta.consultarSaldo();
+        } else {
+            System.out.println("Nao foi possivel realizar saque. Saldo insuficiente.");
+        }
+        saqueEfetuado = conta.realizarSaque(500);
+        System.out.println("Tentativa de saque de 500 reais");
+        if (saqueEfetuado){
+            System.out.println("Saque efetuado com sucesso");
+            conta.consultarSaldo();
+        } else {
+            System.out.println("Nao foi possivel realizar saque. Saldo insuficiente.");
+        }
+
+        System.out.println("Deposito de 500 reais");
+        conta.depositar(500);
+        conta.consultarSaldo();
+
+        if (conta.verificarUsoChequeEspecial()){
+            System.out.println("Esta usando cheque especial");
+        } else {
+            System.out.println("Nao esta Usando cheque especial");
+        }
+        conta.realizarSaque(600);
+        conta.consultarSaldo();
+        if (conta.verificarUsoChequeEspecial()){
+            System.out.println("Esta usando cheque especial");
+        } else {
+            System.out.println("Nao esta Usando cheque especial");
+        }
+    }
+}
