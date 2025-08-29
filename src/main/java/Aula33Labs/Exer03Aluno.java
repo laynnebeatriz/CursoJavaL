849b0b4f225d04c2b0f117ab3d4169f3c2c3aaa2
laynnebeatriz <beatrizmorais132@gmail.com>
2025-08-29 18:08:19 -0300

    03 - Correção dos exercicios da aula 33 - OO Aluno

---
+++ src/main/java/Aula33Labs/Exer03Aluno.java
@@ -0,0 +1,42 @@
+package Aula33Labs;
+
+import java.util.Scanner;
+
+public class Exer03Aluno {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        Aluno aluno = new Aluno();
+
+        System.out.println("Entre com o nome do Aluno");
+        aluno.setNome(scan.next());
+
+        System.out.println("Entre com o nome do Curso");
+        aluno.setNomeCurso(scan.next());
+
+        System.out.println("Entre com a Matricula");
+        aluno.setMatricula(scan.next());
+
+        //aluno.nomeDisciplinas = new String[3];
+        for (int i = 0; i < aluno.getNomeDisciplinas().length; i++ ){
+            System.out.println("Entre com o nome da disciplina " + i);
+            aluno.setNomeDisciplinaPos(i, scan.next());
+        }
+        for (int i = 0; i < aluno.getNotasDisciplinas().length; i++){
+            System.out.println("Obtendo notas da disciplina " + aluno.getNomeDisciplinas()[i]);
+            for (int j = 0; j < aluno.getNotasDisciplinas()[i].length; j++){
+                System.out.println("Entre com a nota " + (j+1));
+                aluno.setNomePosIJ(i,j,scan.nextDouble());
+            }
+        }
+        aluno.mostrarInfo();
+        for (int i = 0; i <  aluno.getNomeDisciplinas().length; i++ ){
+            if (aluno.verificarAprovado(i)){
+                System.out.println("Disciplina " + aluno.getNomeDisciplinas()[i] + " - foi aprovado");
+            } else {
+                System.out.println("Disciplina " + aluno.getNomeDisciplinas()[i] + " - foi reprovado");
+            }
+        }
+    }
+}
