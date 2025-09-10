3217461cb10426d50e7f812bdf819681277d7328
laynnebeatriz <beatrizmorais132@gmail.com>
2025-09-10 17:26:19 -0300

    03 - Correção dos exercicios da aula 34 - OO Calculadora Fatorial

---
+++ src/main/java/Aula34Labs/Exer03.java
@@ -0,0 +1,23 @@
+package Aula34Labs;
+
+import java.util.Scanner;
+
+public class Exer03 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+
+        do{
+            System.out.println("Entre com um nuemero positivo");
+            num = scan.nextInt();
+
+            if (num < 0){
+                System.out.println("Numero invalido, entre novamente");
+            }
+        } while (num < 0);
+
+        System.out.println(Calculadora.fatorial(num));
+    }
+}
