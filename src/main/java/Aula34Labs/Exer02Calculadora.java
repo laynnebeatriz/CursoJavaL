e2df65a2d3dc690474ae993db7f423cd4c1191c7
laynnebeatriz <beatrizmorais132@gmail.com>
2025-09-10 16:42:04 -0300

    02 - Correção dos exercicios da aula 34 - OO Calculadora

---
+++ src/main/java/Aula34Labs/Exer02Calculadora.java
@@ -0,0 +1,19 @@
+package Aula34Labs;
+
+public class Exer02Calculadora {
+    public static void main(String[] args) {
+
+        imprimirTela(Calculadora.somar(1,2));
+
+        imprimirTela(Calculadora.subtrair(2, 1));
+
+        imprimirTela(Calculadora.multiplicar(2, 2));
+
+        imprimirTela(Calculadora.dividir(4, 2));
+
+        imprimirTela(Calculadora.potencia(2, 4));
+    }
+    static void imprimirTela(int num){
+        System.out.println(num);
+    }
+}
