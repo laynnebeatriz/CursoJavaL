a4a19775bfcea60533871131f949a2fa306c25a4
laynnebeatriz <beatrizmorais132@gmail.com>
2025-09-11 17:41:36 -0300

    Fibonacci Recursivo - Exerc 01 - Correção de exercicio

---
+++ src/main/java/Aula35labs/Exer01.java
@@ -0,0 +1,10 @@
+package Aula35labs;
+
+public class Exer01 {
+    public static void main(String[] args) {
+
+        for (int i=0; i<20; i++){
+            System.out.println(Calculadora.fibonacci(i) + " ");
+        }
+    }
+}
